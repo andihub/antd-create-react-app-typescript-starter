9e964894e2d46bd6ed99823df350ddc42374e4a3
andihub <3052211+andihub@users.noreply.github.com>
2019-10-19 21:16:43 +0200

    setup Ant Design, TypeScript and minor tweaks
    based on https://ant.design/docs/react/use-in-typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Row, Col, Button, message } from "antd";
+import "./App.css";
 
-const App: React.FC = () => {
+const App = () => {
+  const onClick = () => {
+    message.info("Hello, World!");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row
+      type="flex"
+      justify="center"
+      align="middle"
+      style={{ height: "100vh" }}
+    >
+      <Col>
+        <Button type="primary" size="large" onClick={onClick}>
+          Say Hello
+        </Button>
+      </Col>
+    </Row>
   );
-}
+};
 
 export default App;
